50971435339c451843ada7c0068c912dd427cfa9
twinjke <seventeensasha7@gmail.com>
2023-10-22 12:15:46 +0300

    feat: finish-task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+      return [];
+  }
+  return matrix.map((el, i) => (i % 2 ? el.reverse() : el)).flat();
+};
+
